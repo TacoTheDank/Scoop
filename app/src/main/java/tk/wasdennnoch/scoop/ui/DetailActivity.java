e042d3494b2ae1064af92bdcfc0e7aa4c586dc88
wasdennnoch <leiter04@gmail.com>
2016-12-24 23:30:12 +0100

    Option to limit line width, search through stack trace

--- app/src/main/java/tk/wasdennnoch/scoop/ui/DetailActivity.java
+++ app/src/main/java/tk/wasdennnoch/scoop/ui/DetailActivity.java
@@ -3,9 +3,18 @@ package tk.wasdennnoch.scoop.ui;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import android.text.style.BackgroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -14,12 +23,15 @@ import android.widget.Toast;
 import tk.wasdennnoch.scoop.R;
 import tk.wasdennnoch.scoop.data.Crash;
 import tk.wasdennnoch.scoop.data.CrashLoader;
+import tk.wasdennnoch.scoop.view.CroppingScrollView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
 
-    public static final String EXTRA_CRASH = "stacktrace";
+    public static final String EXTRA_CRASH = "crash";
 
+    private TextView mCrashText;
     private Crash mCrash;
+    private int mHighlightColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +41,55 @@ public class DetailActivity extends AppCompatActivity {
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mHighlightColor = ContextCompat.getColor(this, R.color.highlightColor);
         mCrash = getIntent().getParcelableExtra(EXTRA_CRASH);
         getSupportActionBar().setTitle(CrashLoader.getAppName(this, mCrash.packageName, true));
-        TextView crashText = (TextView) findViewById(R.id.crash);
-        crashText.setText(mCrash.stackTrace);
+        mCrashText = (TextView) findViewById(R.id.crash);
+        mCrashText.setText(mCrash.stackTrace);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        ((CroppingScrollView) findViewById(R.id.scroll)).setCropHorizontally(prefs.getBoolean("auto_wrap", false));
+    }
+
+    private void highlightText(String text) {
+        text = text.toLowerCase(); // Ignore case
+        final String stackTrace = mCrash.stackTrace.toLowerCase();
+        SpannableString span = new SpannableString(mCrash.stackTrace);
+        if (!TextUtils.isEmpty(text)) {
+            final int size = text.length();
+            int index = 0;
+            while ((index = stackTrace.indexOf(text, index)) != -1) {
+                span.setSpan(new BackgroundColorSpan(mHighlightColor), index, index + size, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                index += size;
+            }
+        }
+        mCrashText.setText(span);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnCloseListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onClose() {
+        highlightText(null);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        highlightText(newText);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
         return true;
     }
 
@@ -63,5 +115,4 @@ public class DetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
---
+++ app/src/main/java/tk/wasdennnoch/scoop/view/CroppingScrollView.java
@@ -0,0 +1,56 @@
+package tk.wasdennnoch.scoop.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class CroppingScrollView extends MergedScrollView {
+
+    private boolean mCropHorizontally;
+
+    public CroppingScrollView(Context context) {
+        this(context, null);
+    }
+
+    public CroppingScrollView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CroppingScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mCropHorizontally = false;
+    }
+
+    public void setCropHorizontally(boolean crop) {
+        if (mCropHorizontally != crop) {
+            mCropHorizontally = crop;
+            requestLayout();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (!mCropHorizontally) return;
+
+        if (getChildCount() > 0) {
+            final View child = getChildAt(0);
+            final int widthPadding;
+            final int targetSdkVersion = getContext().getApplicationInfo().targetSdkVersion;
+            final FrameLayout.LayoutParams lp = (LayoutParams) child.getLayoutParams();
+            if (targetSdkVersion >= Build.VERSION_CODES.M) {
+                widthPadding = getPaddingLeft() + getPaddingRight() + lp.leftMargin + lp.rightMargin;
+            } else {
+                widthPadding = getPaddingLeft() + getPaddingRight();
+            }
+            final int desiredWidth = getMeasuredWidth() - widthPadding;
+
+            if (mCropHorizontally && child.getMeasuredWidth() > desiredWidth) {
+                child.measure(MeasureSpec.makeMeasureSpec(desiredWidth, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            }
+        }
+    }
+}
