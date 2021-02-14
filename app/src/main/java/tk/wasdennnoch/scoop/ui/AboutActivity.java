1f908532757209abe960663fcc22ec7c42ae5873
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-02-14 17:45:06 -0500

    Add an update check button in the about page

--- app/src/main/java/tk/wasdennnoch/scoop/ui/AboutActivity.java
+++ app/src/main/java/tk/wasdennnoch/scoop/ui/AboutActivity.java
@@ -1,12 +1,15 @@
 package tk.wasdennnoch.scoop.ui;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.pm.PackageInfoCompat;
 
 import tk.wasdennnoch.scoop.R;
@@ -20,6 +23,8 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar(findViewById(R.id.about_toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        AppCompatButton checkUpdate = findViewById(R.id.about_updates);
+
         try {
             PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
             ((TextView) findViewById(R.id.about_version))
@@ -31,7 +36,12 @@ public class AboutActivity extends AppCompatActivity {
         }
 
         ((TextView) findViewById(R.id.about_credits)).setText(getString(R.string.about_author,
-                "@MrWasdennnoch (XDA), @paphonb (XDA)"));
+                "@MrWasdennnoch (XDA), @paphonb (XDA), @TacoTheDank (GitHub)"));
+
+        checkUpdate.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/TacoTheDank/Scoop/releases"));
+            startActivity(i);
+        });
     }
 
     @Override
