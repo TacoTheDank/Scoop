a258718853dc63ebcb35343ca77f26967c4b4752
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-07-22 13:51:45 -0400

    Use NotificationChannelCompat features

--- app/src/main/java/taco/scoop/ScoopApplication.kt
+++ app/src/main/java/taco/scoop/ScoopApplication.kt
@@ -1,12 +1,9 @@
 package taco.scoop
 
-import android.annotation.TargetApi
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.SystemClock
-import androidx.core.content.getSystemService
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
 import taco.scoop.util.PreferenceHelper.initPreferences
 
 class ScoopApplication : Application() {
@@ -15,34 +12,24 @@ class ScoopApplication : Application() {
         super.onCreate()
 
         initPreferences()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            registerCrashesChannel()
-            registerStatusChannel()
-        }
+        registerChannels()
     }
 
-    @TargetApi(Build.VERSION_CODES.O)
-    private fun registerCrashesChannel() {
-        val name = getString(R.string.crash_channel)
-        val importance = NotificationManager.IMPORTANCE_HIGH
-        val channel = NotificationChannel("crashes", name, importance)
-        channel.createNotifChannel()
-    }
+    private fun registerChannels() {
+        val crashesChannel =
+            NotificationChannelCompat.Builder("crashes", NotificationManagerCompat.IMPORTANCE_HIGH)
+                .setName(getString(R.string.crash_channel))
+                .build()
 
-    @TargetApi(Build.VERSION_CODES.O)
-    private fun registerStatusChannel() {
-        val name = getString(R.string.status_channel)
-        val importance = NotificationManager.IMPORTANCE_MIN
-        val channel = NotificationChannel("status", name, importance)
-        channel.setShowBadge(false)
-        channel.createNotifChannel()
-    }
+        val statusChannel =
+            NotificationChannelCompat.Builder("status", NotificationManagerCompat.IMPORTANCE_MIN)
+                .setName(getString(R.string.status_channel))
+                .setShowBadge(false)
+                .build()
 
-    @TargetApi(Build.VERSION_CODES.O)
-    fun NotificationChannel.createNotifChannel() {
-        val notificationManager = getSystemService<NotificationManager>()
-        notificationManager?.createNotificationChannel(this)
+        val notificationManager = NotificationManagerCompat.from(this)
+        val channelArray = mutableListOf(crashesChannel, statusChannel)
+        notificationManager.createNotificationChannelsCompat(channelArray)
     }
 
     companion object {
